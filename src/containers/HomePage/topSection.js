938ac99a23081a1a96f65d45e362789e785ddbdc
hackhim18 <triphimanshu18@gmail.com>
2021-07-08 12:09:03 +0530

    08/07 -c2

--- src/containers/HomePage/topSection.js
+++ src/containers/HomePage/topSection.js
@@ -1,25 +1,36 @@
-import React from 'react'
-import styled from 'styled-components';
-import TopSectionBackgroundImg from "../../images/landing-page.jpg"
-import bestimg from "../../images/Work only with the best.png";
-import { BrandLogo } from '../../components/brandLogo';
+import React from "react";
+import { useMediaQuery } from "react-responsive";
+import styled from "styled-components";
+import { BrandLogo } from "../../components/brandLogo";
+import { Button } from "../../components/button";
 import { Marginer } from "../../components/marginer";
+import { deviceSize } from "../../components/responsive";
+//import { resposive }  from "../../components/responsive";
+
+
+import TopSectionBackgroundImg from "../../images/landing-page.jpg";
+import TheBestSpecialistsImg from "../../images/Work only with the best.png";
 
 const TopSectionContainer = styled.div`
   width: 100%;
-  height:700px;
-  background: url(${TopSectionBackgroundImg});
-  background-postion: 0px -80px;
-  background-size:cover;
+  height: 800px; 
+  background: url(${TopSectionBackgroundImg}) no-repeat;
+  background-position: 0px -150px;
+  background-size: cover;
+  @media screen and (max-width: ${deviceSize.mobile}px) {
+    height: 700px;
+    background-position: 0px 0px;
+  } 
 `;
-  
+
 const BackgroundFilter = styled.div`
-width:100%;
-height:100%;
-background-color: rgba(38, 70, 83, 0.9);
-display: flex;
-flex-direction: column;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(38, 70, 83, 0.9);
+  display: flex;
+  flex-direction: column;
 `;
+
 const TopSectionInnerContainer = styled.div`
   width: 100%;
   height: 100%;
@@ -27,8 +38,6 @@ const TopSectionInnerContainer = styled.div`
   align-items: center;
   justify-content: space-evenly;
 `;
-
-
 
 const StandoutImage = styled.div`
   width: 44em;
@@ -38,43 +47,55 @@ const StandoutImage = styled.div`
     height: 100%;
   }
 `;
- 
+
+const LogoContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  @media screen and (max-width: ${deviceSize.mobile}px) {
+    align-items: center;
+  }
+`;
 
 const SloganText = styled.h3`
   margin: 0;
   line-height: 1.4;
   color: #fff;
   font-weight: 500;
-  font-size: 40x;
-   
-    font-size: 40px;
-  
-`;
- 
-const LogoContainer = styled.div`
-display:flex;
-flex-direction:column;
-align-items: flex-start;
+  font-size: 35px;
+  @media screen and (max-width: ${deviceSize.mobile}px) {
+    font-size: 24px;
+  }
 `;
 
- 
- 
-export function TopSection(props){
-return  ( <TopSectionContainer>
-  <BackgroundFilter>
-    <TopSectionInnerContainer> 
-      <LogoContainer>
-          <BrandLogo logoSize={65} textSize={55}/>
-          <Marginer direction="vertical" margin={15} />
-          <SloganText>Find the right person </SloganText>
-          <SloganText>For your job!</SloganText>
+export function TopSection(props) {
+  const { children } = props;
 
-      </LogoContainer>
-    <StandoutImage>
-       <img src={bestimg}  alt="best in the field"/>
-   </StandoutImage>
-   </TopSectionInnerContainer>  
-  </BackgroundFilter>
-</TopSectionContainer>
-);
+  const isMobile = useMediaQuery({ maxWidth: deviceSize.mobile });
+
+  return (
+    <TopSectionContainer>
+      <BackgroundFilter>
+        {children}
+        <TopSectionInnerContainer>
+          <LogoContainer>
+            <BrandLogo
+              logoSize={isMobile ? 40 : 65}
+              textSize={isMobile ? 35 : 55}
+            />
+            <Marginer direction="vertical" margin={8} />
+            <SloganText>Find the right specialist</SloganText>
+            <SloganText>For the job</SloganText>
+            <Marginer direction="vertical" margin={15} />
+            <Button>Join Now</Button>
+          </LogoContainer>
+          {!isMobile && (
+            <StandoutImage>
+              <img src={TheBestSpecialistsImg} alt="best in the field" />
+            </StandoutImage>
+          )}
+        </TopSectionInnerContainer>
+      </BackgroundFilter>
+    </TopSectionContainer>
+  );
 }
