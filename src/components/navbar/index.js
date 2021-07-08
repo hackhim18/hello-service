aca6cb06aa3028c275136625517f5d8060a91724
hackhim18 <triphimanshu18@gmail.com>
2021-07-08 14:04:02 +0530

    08/07 -c5

--- src/components/navbar/index.js
+++ src/components/navbar/index.js
@@ -1,32 +1,46 @@
  import React from 'react'
 import styled from 'styled-components'
  import { BrandLogo } from '../brandLogo'
- 
- const NavbarContainer = styled.div`
- width: 100%;
- height: 85px;
- display: flex;
- align-items: center;
- justify-content: space-between;
- `;
+ import { Button } from "../button";
+ import { Marginer } from "../marginer";
 
-const AccessiblityContainer = styled.div`
-display: flex;
+
+ const NavbarContainer = styled.div`
+  width: 100%;
+  height: 65px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 1.5em;
+  background-color: ${({ useTransparent }) =>
+    useTransparent ? "transparent" : "#264653"};
+`;
+
+const AccessibilityContainer = styled.div`
+  height: 100%;
+  display: flex;
+  align-items: center;
 `;
 
 const AnchorLink = styled.a`
- font-size: 14px;
- color: #fff;
- cursor: pointer;
- text-decoration: none;
- outline: none;
- font-weight: bold;
- transition: all 200ms ease-in-out;
-
- &:hover{
-   filter:contrast(0.6);
- }
+  font-size: 15px;
+  color: #fff;
+  cursor: pointer;
+  font-weight: inherit;
+  text-decoration: none;
+  outline: none;
+  transition: all 200ms ease-in-out;
+  &:hover {
+    filter: contrast(0.6);
+  }
 `;
+
+const Seperator = styled.div`
+  min-height: 35%;
+  width: 1px;
+  background-color: #fff;
+`;
+
 
 
 
@@ -34,12 +48,15 @@ const AnchorLink = styled.a`
    return (
       <NavbarContainer>
         <BrandLogo />
-        <AccessiblityContainer>
+      <AccessibilityContainer>
 <AnchorLink>Specialist Portal</AnchorLink>
- 
- <AnchorLink>Specialist Portal</AnchorLink>
-
-      </AccessiblityContainer>
+<Marginer direction="horizontal" margin={10} />  
+  <Seperator />
+  <Marginer direction="horizontal" margin={10} />
+  <Button size={13}>Register</Button>
+  <Marginer direction="horizontal" margin={10} />  
+ <AnchorLink>Login</AnchorLink>
+      </AccessibilityContainer>
       </NavbarContainer>
    )
  }
